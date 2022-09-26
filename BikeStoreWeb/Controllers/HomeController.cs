9f3dffd10a83197e61f944419d2510bdd7cde058
nhatminhpham772 <90132632+nhatminhpham772@users.noreply.github.com>
2022-09-26 17:24:11 +0700

    test6

--- BikeStoreWeb/Controllers/HomeController.cs
+++ BikeStoreWeb/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace BikeStoreWeb.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application ";
+            ViewBag.Message = "";
 
             return View();
         }
